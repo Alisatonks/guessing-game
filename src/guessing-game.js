ebb52708ebf84f0629b99b536e3cda2e32c90c4e
Volha Haiduk <Alisatonks2385@gmail.com>
2022-08-16 18:14:27 +0200

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        
     }
 
     guess() {
-
+        return Math.round((this.max - this.min) / 2 + this.min)
     }
 
     lower() {
-
+      
+        this.max = Math.round((this.max - this.min) / 2 + this.min)
     }
 
     greater() {
-
+      
+        this.min =  Math.round((this.max - this.min) / 2 + this.min)
     }
 }
 
